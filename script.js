e02048d3f6848f2f98dba1332df78a809ce39689
Matheus Silva <msantossilva005@gmail.com>
2023-03-15 23:25:56 -0300

    img raw part 2

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ fetch("https://raw.githubusercontent.com/theussant/IC-MPDS/master/assets/sistema
 
 
 // Requisição do arquivo RAW do GitHub usando a função "fetch" do JavaScript.
+
+
+function carregarImagem(url) {
+	var canvas = document.getElementById('raw');
+	var context = canvas.getContext('2d');
+	var request = new XMLHttpRequest();
+	request.open('GET', url, true);
+	request.responseType = 'arraybuffer';
+	request.onload = function() {
+		var bytes = new Uint8ClampedArray(request.response);
+		var imageData = new ImageData(bytes, largura, altura);
+		canvas.width = 557;
+		canvas.height = 516;
+		context.putImageData(imageData, 0, 0);
+	};
+	request.send();
+}
+
+carregarImagem('https://raw.githubusercontent.com/theussant/IC-MPDS/master/assets/sistema.PNG');
