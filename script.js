85e9a78783b5eb801b5844ce90665b0ab27320c1
Matheus Silva <msantossilva005@gmail.com>
2023-05-31 17:48:49 -0300

    Remoção de código morto

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-fetch("https://raw.githubusercontent.com/theussant/IC-MPDS/master/response.txt")
-        .then(response => response.text())
-        .then(data => {
-            document.querySelector("#raw-content").textContent = data;
-        });
-
-
 // Requisição do arquivo RAW do GitHub usando a função "fetch" do JavaScript.
 
   const user = "theussant";
